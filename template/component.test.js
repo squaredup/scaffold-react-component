0b91c0c6828be18fe3fe87fc3a1880ee1869ce06
Dave Clarke <dave.clarke@squaredup.com>
2020-04-01 17:54:47 +0100

    Change test to todo test

--- template/component.test.js
+++ template/component.test.js
@@ -3,8 +3,4 @@ import { render  } from '@testing-library/react';
 import __NAME__ from './__NAME__';
 import styles from './__NAME__.module.scss';
 
-test('__NAME__ renders some content', () => {
-    const { getByText } = render(<__NAME__>Hello</__NAME__>);
-    const component = getByText('Hello');
-    expect(component).toBeInTheDocument();
-});
+test.todo('__NAME__ renders succesfully');

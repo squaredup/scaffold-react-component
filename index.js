2f176c0d4eac4797389dac683bdec6b2d2208f43
Dave Clarke <dave.clarke@squaredup.com>
2020-04-01 18:08:57 +0100

    Add colours & icons to output!

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 const path = require('path');
 const yargs = require('yargs');
+const chalk = require('chalk');
+const symbols = require('log-symbols');
 const util = require('./util.js');
 
 const StubName = "__NAME__";
@@ -10,12 +12,12 @@ const StubStoryBookType = "__SB_TYPE__";
 const ComponentClassSrcPath = 'template/component-class.js';
 const ComponentFunctionalSrcPath = 'template/component-functional.js';
 const StyleSrcPath = 'template/component.module.scss';
-const StorySrcPath = 'template/component.story.js';
+const StorySrcPath = 'template/component.stories.js';
 const TestSrcPath = 'template/component.test.js';
 
 const ComponentTargetPath = (name) => `${name}.js`;
 const StyleTargetPath = (name) => `${name}.module.scss`;
-const StoryTargetPath = (name) => `${name}.story.js`;
+const StoryTargetPath = (name) => `${name}.stories.js`;
 const TestTargetPath = (name) => `${name}.test.js`;
 
 const ConstructMap = (name, type, storybookType) => {
@@ -26,8 +28,14 @@ const ConstructMap = (name, type, storybookType) => {
     }
 };
 
+const log = {
+    ok: (text) => console.log(`${symbols.success} ${chalk.green(text)}`),
+    err: (text) => console.log(`${symbols.error} ${chalk.red(text)}`),
+    info: (text) => console.log(`${chalk.blue(text)}`)
+};
+
 const scaffold = (name, type, storybookType) => {
-    console.log(`Scaffolding component ${name} (${type})`);
+    log.info(`Scaffolding component ${name} (${type})`);
 
     const component = ComponentTargetPath(name);
 
@@ -39,7 +47,7 @@ const scaffold = (name, type, storybookType) => {
                 path.resolve(process.cwd(), component), 
                 ConstructMap(name, type, storybookType)
             );
-            console.log(`Created functional component ${component}`);
+            log.ok(`Created functional component ${chalk.green.underline.bold(component)}`);
             break;
 
         case 'class':
@@ -48,7 +56,7 @@ const scaffold = (name, type, storybookType) => {
                 path.resolve(process.cwd(), component), 
                 ConstructMap(name, type, storybookType)
             );
-            console.log(`Created class component ${component}`);
+            log.ok(`Created class component ${chalk.green.underline.bold(component)}`);
             break;
 
     }
@@ -59,7 +67,7 @@ const scaffold = (name, type, storybookType) => {
         path.resolve(process.cwd(), StyleTargetPath(name)), 
         ConstructMap(name, type, storybookType)
     );
-    console.log(`Created SCSS module for component ${StyleTargetPath(name)}`);
+    log.ok(`Created SCSS module for component ${chalk.green.underline.bold(StyleTargetPath(name))}`);
 
     // Story
     util.readReplaceWriteAsync(
@@ -67,7 +75,7 @@ const scaffold = (name, type, storybookType) => {
         path.resolve(process.cwd(), StoryTargetPath(name)), 
         ConstructMap(name, type, storybookType)
     );
-    console.log(`Created Story for component ${StoryTargetPath(name)}`);
+    log.ok(`Created Story for component ${chalk.green.underline.bold(StoryTargetPath(name))}`);
 
     // Test
     util.readReplaceWriteAsync(
@@ -75,42 +83,57 @@ const scaffold = (name, type, storybookType) => {
         path.resolve(process.cwd(), TestTargetPath(name)), 
         ConstructMap(name, type, storybookType)
     );
-    console.log(`Created Test for component ${TestTargetPath(name)}`);
+    log.ok(`Created Test for component ${chalk.green.underline.bold(TestTargetPath(name))}`);
 }
 
 yargs
-    .command('$0 [name] [storybookCategory]', 'Scaffold a React component with styles, tests and a story.', () => {}, (argv) => {
-        scaffold(argv.name, argv.type, argv.sb);
+    .scriptName('scaffold')
+    .command(
+        '$0 [name] [type] [storybookCategory]', 
+        chalk.green.bold('Scaffold a React component with styles, tests and a story.'), 
+        (y) => {
+            y.example('$0', 'ActionPanel');
+            y.example('$0', 'ActionPanel class');
+            y.example('$0', 'ActionPanel --type class');
+            y.example('$0', 'ActionPanel Common');
+            y.example('$0', 'ActionPanel --sb Common');
+        }, 
+        (argv) => {
+            try {
+                scaffold(argv.name, argv.type, argv.sb);
+                log.info(chalk.bold("Scaffolding complete!"))
+            } catch (ex) {
+                log.err(ex);
+            }
     })
     .option('name', {
         alias: 'n',
         type: 'string',
-        description: 'The name of the component to scaffold',
+        description: chalk.blue('The name of the component to scaffold'),
         demandOption: true
     })
     .option('type', {
         alias: 't',
         type: 'string',
-        description: 'Type of the React component',
+        description: chalk.blue('Type of the React component'),
         choices: ['functional', 'class'],
         default: 'functional'
     })
     .option('storybookCategory', {
         alias: 'sb',
         type: 'string',
-        description: 'Category for the Story to use',
+        description: chalk.blue('Category for the Story to use'),
         default: 'Common'
     })
     .check((argv) => {
         //check first letter of name is a capital
         if(!(argv.name.charCodeAt(0) >= 65 && argv.name.charCodeAt(0) <= 90)) {
-            throw new Error('Error: The component name should be capitalised.');
+            log.err('Error: The component name should be capitalised.');
+            false;
         }; 
 
         return true;
     })
-    .example('$0', 'e.g. scaffold ActionPanel')
-    .example('$0', 'e.g. scaffold ActionPanel --type class')
-    .example('$0', 'e.g. scaffold ActionPanel --storybookCategory Common')
+    .wrap(yargs.terminalWidth())   
     .help()
     .argv;
